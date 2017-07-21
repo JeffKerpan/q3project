4cc0ed421c01ab8e3e81fbcb731cb5f6f06f99d8
wallytreats <nicklawrenceemail@gmail.com>
2017-07-21 14:17:55 -0600

    seperated the style as a seperate component

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Comp1 from './components/Comp1.js';
+import style from './styles/stylecomp.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={style.container}>
         <Text>DRINK WATER</Text>
-        <Text>How to start: Open up App.js to start working on the project</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Comp1 />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Comp1.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+
+class Comp1 extends Component {
+
+  render() {
+    return (
+      <View>
+        <Text>IM A COMPONENT</Text>
+      </View>
+    );
+  }
+}
+
+export default Comp1;
