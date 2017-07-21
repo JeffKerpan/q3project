9d09b3dc813eea23d0911e40474194e3765fda5a
wallytreats <nicklawrenceemail@gmail.com>
2017-07-21 12:58:04 -0600

    added creat-react-native-app

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>DRINK WATER</Text>
+        <Text>How to start: Open up App.js to start working on the project</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
