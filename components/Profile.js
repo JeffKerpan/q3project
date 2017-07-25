1b538100d3c02b9a62179810c9d2bfb253b6806f
Brennen Bull <brennenbull@gmail.com>
2017-07-24 19:23:03 -0600

    add view layout for profile page

--- components/Profile.js
+++ components/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button } from 'react-native';
+import { TextInput, ScrollView, AppRegistry, Button, StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 export default class Profile extends Component {
@@ -53,18 +53,32 @@ export default class Profile extends Component {
 
   render() {
     return (
-      <View>
-        <Text>IM A COMPONENT</Text>
-        <Text>{this.props.navigation.state.params.userId}</Text>
-        <Button onPress = {this.onSubmit}
-        title="button"
-        color="#841584"
-        />
-        <Button
-        onPress= { () => {this.props.navigation.navigate('Home')}}
-        title="Logout"
-        color="#841584"/>
-      </View>
+      <ScrollView> //Page Level Root
+        <View>//This start upper third flex 1
+          <View> //this is the counter flex 1
+            <Text>{this.state.newamount}</Text>
+          </View>//this ends counter
+          <View>//This is dropper and counter flex 2 and flex row
+            <View>//this is the water drop flex 2
+              <Text>{this.props.navigation.state.params.userId}</Text>
+            </View>//this ends water drop
+            <View>//this is the +/- flex 1 flex
+              <Button onPress = {this.onSubmit}
+              title="button"
+              color="#841584"
+              />
+            </View>//this ends +/-
+          </View>//this ends drop and counter
+        </View>//this ends upper third flex 1
+        <View>//this starts middle third flex 1
+          <Button
+          onPress= { () => {this.props.navigation.navigate('Home')}}
+          title="Logout"
+          color="#841584"/>
+        </View>//this ends middle third
+        <View>//this starts bottom third flex 1
+        </View>//this ends bottom third
+      </ScrollView>
     );
   }
 }
